19fae5f071256bb6d5bc910dd83088d2ac64f152
Teresa Saller <tsaller@lcs.on.ca>
2019-11-21 16:38:04 -0500

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Saller, Teresa on 2019-11-21.
+//  Copyright © 2019 Saller, Teresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
